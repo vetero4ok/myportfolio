f5a1438acc7d5cd7cb67c8e49bf959ba621fe420
vetero4ok <vetero4ok@gmail.com>
2021-10-16 14:15:01 +0300

    refactor and install react router dom and node sass

--- src/projects/work/Work.js
+++ src/projects/work/Work.js
@@ -1,16 +1,18 @@
 import s from './Work.module.css';
 import {Button} from "../../common/Button/Button";
-    export function Work(props) {
+
+export function Work(props) {
     return (
         <div className={s.work}>
             <div className={s.image} style={{backgroundImage: `url(${props.img})`}}>
-                <Button nameButton={props.nameButton}/>
+                <a className={s.viewBtn}>
+                    <Button nameButton={props.nameButton} className={s.viewBtn}/>
+                </a>
             </div>
             {/*<div className={s.descriptionMenu}>*/}
             {/*    <h3 className={s.workTitle}>{props.title}</h3>*/}
             {/*    <p>{props.description}</p>*/}
             {/*</div>*/}
-
         </div>
     );
 
