78e2f15db038153244d79de70e5896a97495beb0
vetero4ok <vetero4ok@gmail.com>
2021-10-17 15:59:40 +0300

    lending main

--- src/main/Main.js
+++ src/main/Main.js
@@ -5,16 +5,24 @@ import my from '../common/img/my.jpg'
 export function Main() {
     return (
         <div className={s.mainBlock}>
-            <div className={`${styleContainer.container} `}>
-                <div className={s.photo}>
-                    <img src={my} alt={''}/>
-                </div>
-                <div className={s.text}>
-                    <span>Hi there!</span>
-                    <div className={s.titleName}>
-                        <h4> I am Igor Svyrydovskyi</h4>
+            <div className={`  ${styleContainer.container} `}>
+                <div className={`${s.mainContainer}`}>
+                    <div className={s.photo}>
+                        <img src={my} alt={''}/>
                     </div>
-                    <h2>Front-end Developer</h2>
+                    <div className={s.text}>
+                        <h3> I'm Igor Svyrydovskyi</h3>
+                        <h2>Front - end Developer</h2>
+                        <div className={s.info}><p>
+                            Hello, I'm a person who really wants to became a good programmer. I have an experience in
+                            creating SPA with React/Redux/TypeScript. Now I'm improving my skills in this direction and
+                            expanding them with new technologies. I'm in process of improving my level of Node.js,
+                            because
+                            my purpose is to become a advanced Full-Stack Developer. I spend my free time on Codewars or
+                            with technical documentation. I'll do all my best to achieve my goals. I'm open for your
+                            suggestions
+                        </p></div>
+                    </div>
                 </div>
             </div>
         </div>);
