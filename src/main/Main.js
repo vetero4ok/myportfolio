b7f83cdd818585ddd5f03e1516fd74769d001ae1
vetero4ok <vetero4ok@gmail.com>
2021-10-18 18:38:58 +0300

    main sass

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import s from './Main.module.css';
+import s from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css'
 import my from '../common/img/my.jpg'
 
@@ -13,15 +13,20 @@ export function Main() {
                     <div className={s.text}>
                         <h3> I'm Igor Svyrydovskyi</h3>
                         <h2>Front - end Developer</h2>
-                        <div className={s.info}><p>
-                            Hello, I'm a person who really wants to became a good programmer. I have an experience in
-                            creating SPA with React/Redux/TypeScript. Now I'm improving my skills in this direction and
-                            expanding them with new technologies. I'm in process of improving my level of Node.js,
-                            because
-                            my purpose is to become a advanced Full-Stack Developer. I spend my free time on Codewars or
-                            with technical documentation. I'll do all my best to achieve my goals. I'm open for your
-                            suggestions
-                        </p></div>
+                        <div className={s.info}>
+                            <p>
+                                Hello, I'm a person who really wants to became a good programmer. I have an experience
+                                in
+                                creating SPA with React/Redux/TypeScript. Now I'm improving my skills in this direction
+                                and
+                                expanding them with new technologies. I'm in process of improving my level of Node.js,
+                                because
+                                my purpose is to become a advanced Full-Stack Developer. I spend my free time on
+                                Codewars or
+                                with technical documentation. I'll do all my best to achieve my goals. I'm open for your
+                                suggestions
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
